c862fc06b0e3e8abe52baf5d25a0a2c47afe45dc
Armando Picón <picondev@gmail.com>
2017-11-04 13:32:34 -0300

    Initial commit

---
+++ app/src/main/java/pe/devpicon/android/foodstorage/addproduct/AddProductActivity.kt
@@ -0,0 +1,13 @@
+package pe.devpicon.android.foodstorage.addproduct
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import pe.devpicon.android.foodstorage.R
+
+class AddProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}
